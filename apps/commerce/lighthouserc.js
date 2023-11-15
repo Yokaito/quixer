d0b35612476bc35e7580e6125382d4c9c47fd968
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-11-15 11:10:14 -0400

    chore: add staticDistDir to lhci config

--- apps/commerce/lighthouserc.js
+++ apps/commerce/lighthouserc.js
@@ -4,6 +4,7 @@ module.exports = {
   urls: Object.values(lhConfig.pages),
   server: lhConfig.server,
   startServerCommand: lhConfig.startServerCommand,
+  staticDistDir: 'dist/apps/commerce',
   collect: {
     url: Object.values(lhConfig.pages).map((url) => `${lhConfig.server}${url}`)
   },
