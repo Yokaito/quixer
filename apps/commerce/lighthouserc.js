3869701bdcaa3191eeb693a0d22c6bd106883f25
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-11-15 11:14:52 -0400

    chore: change lhci version

--- apps/commerce/lighthouserc.js
+++ apps/commerce/lighthouserc.js
@@ -4,7 +4,6 @@ module.exports = {
   urls: Object.values(lhConfig.pages),
   server: lhConfig.server,
   startServerCommand: lhConfig.startServerCommand,
-  staticDistDir: 'dist/apps/commerce',
   collect: {
     url: Object.values(lhConfig.pages).map((url) => `${lhConfig.server}${url}`)
   },
