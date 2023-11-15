0ee49a24fb58f9429702695bae0488d817b7c6c9
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-11-15 11:38:23 -0400

    chore: add config inside a object ci

--- apps/commerce/lighthouserc.js
+++ apps/commerce/lighthouserc.js
@@ -1,13 +1,15 @@
 const { lighthouse: lhConfig } = require('./store.config');
 
 module.exports = {
-  urls: Object.values(lhConfig.pages),
-  server: lhConfig.server,
-  startServerCommand: lhConfig.startServerCommand,
-  collect: {
-    url: Object.values(lhConfig.pages).map((url) => `${lhConfig.server}${url}`)
-  },
-  upload: {
-    target: 'temporary-public-storage'
+  ci: {
+    urls: Object.values(lhConfig.pages),
+    server: lhConfig.server,
+    startServerCommand: lhConfig.startServerCommand,
+    collect: {
+      url: Object.values(lhConfig.pages).map((url) => `${lhConfig.server}${url}`)
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
   }
 };
