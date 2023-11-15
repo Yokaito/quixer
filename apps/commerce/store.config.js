e422d377d5755854ed44b4af1063a61423dea3d1
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-11-15 11:57:10 -0400

    chore: change config lighthouse

--- apps/commerce/store.config.js
+++ apps/commerce/store.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lighthouse: {
-    startServerCommand: 'pnpm start --filter=commerce',
+    startServerCommand: 'pnpm start',
     server: process.env.BASE_SITE_URL || 'http://localhost:3000',
     pages: {
       home: '/',
