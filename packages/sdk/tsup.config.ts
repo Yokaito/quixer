34db64d49245d9619cc66f8fd47c2fd45e405f4a
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-11-21 23:49:00 -0400

    chore: change files and remove type module in sdk

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, Options } from "tsup";
 export const tsup = defineConfig((options: Options) => ({
   name: "server",
   entry: ["src/index.ts", "src/client.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   outDir: "dist",
   dts: true,
   clean: true,
